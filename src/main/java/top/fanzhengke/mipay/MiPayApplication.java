227ca8aadf8dc249dfac93dde2c7a598abcc1a4e
ZhengKe996 <3119531802@qq.com>
2022-04-01 09:52:50 +0800

    refactor: 项目初始化

---
+++ src/main/java/top/fanzhengke/mipay/MiPayApplication.java
@@ -0,0 +1,13 @@
+package top.fanzhengke.mipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiPayApplication.class, args);
+    }
+
+}
